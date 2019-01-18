e726d73619871b53ac3fe4eb2c71e9ea51ead3e2
kapits <kapits3@yahoo.com>
2019-01-18 12:26:35 +0100

    Add files via upload

--- WebGL/1/js/main.js
+++ WebGL/1/js/main.js
@@ -37,8 +37,6 @@ function getRotation() {
    Z = document.getElementById('rotateZ').checked;
 }
 
-// ==================================================================== //
-
 function gl_getContext (canvas) {
    try {
       var ctx = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -52,15 +50,7 @@ function gl_getContext (canvas) {
    return ctx;
 }
 
-// ==================================================================== //
-
-// Declare the shaders. They are pieces of code compiled by WebGL and
-// executed on the graphics device. They are written in GLSL.
 function gl_initShaders () {
-   // position of the point - 0. is Z and 1. is W
-   // PosMatrix is uniform variable - its value is constant while rendering an object
-   // MovMatrix is the movement matrix of the triangle
-   // gl_position -> we move position with MovMatrix before projecting it
    var vertexShader = "\n\
       attribute vec3 position;\n\
       uniform mat4 PosMatrix;\n\
@@ -73,7 +63,6 @@ function gl_initShaders () {
          vColor = color;\n\
       }";
 
-   // set black color
    var fragmentShader = "\n\
       precision mediump float;\n\
       varying vec3 vColor;\n\
@@ -81,7 +70,6 @@ function gl_initShaders () {
          gl_FragColor = vec4(vColor, 1.);\n\
       }";
 
-   // this function is used to compile a shader
    var getShader = function(source, type, typeString) {
       var shader = gl_ctx.createShader(type);
       gl_ctx.shaderSource(shader, source);
@@ -94,44 +82,28 @@ function gl_initShaders () {
       return shader;
    };
 
-   // Compile the vertex and fragment shaders
    var shader_vertex = getShader(vertexShader, gl_ctx.VERTEX_SHADER, "VERTEX");
    var shader_fragment = getShader(fragmentShader, gl_ctx.FRAGMENT_SHADER, "FRAGMENT");
 
 
-   // Create the Shader program.
-   // Shader program is a combination of a vertex and fragment shaders.
    var SHADER_PROGRAM = gl_ctx.createProgram();
    gl_ctx.attachShader(SHADER_PROGRAM, shader_vertex);
    gl_ctx.attachShader(SHADER_PROGRAM, shader_fragment);
 
-
-   // Linking of the shader program to the WebGL context - gl_ctx,
-   // in order to match the shader variables to javascript variables
    gl_ctx.linkProgram(SHADER_PROGRAM);
 
-   // Link PosMatrix\MovMatrix\ViewMatrix GLSL variables to
-   // _PosMatrix\_MovMatrix\_ViewMatrix javascript variables
-   // Uniforms do not need to be enabled like attributes
    _PosMatrix = gl_ctx.getUniformLocation(SHADER_PROGRAM, "PosMatrix");
    _MovMatrix = gl_ctx.getUniformLocation(SHADER_PROGRAM, "MovMatrix");
    _ViewMatrix = gl_ctx.getUniformLocation(SHADER_PROGRAM, "ViewMatrix");
 
-   // position GLSL variable links to _position variable
-   _position = gl_ctx.getAttribLocation(SHADER_PROGRAM, "position");    // *******
-   // color GLSL variable links to _color variable
+   _position = gl_ctx.getAttribLocation(SHADER_PROGRAM, "position");
    _color = gl_ctx.getAttribLocation(SHADER_PROGRAM, "color");
-   // enable GLSL attributes variables
    gl_ctx.enableVertexAttribArray(_position);
    gl_ctx.enableVertexAttribArray(_color);
-   // linking is over - tells WebGL context to use SHADER_PROGRAM for rendering.
    gl_ctx.useProgram(SHADER_PROGRAM);
 }
 
-// ==================================================================== //
-
 function gl_initBuffers () {
-   // Point coordinates array of the triangle
 //   var triangleVertices = [
 //      -1, -1, 0,    // bottom left
 //       0, 0, 1,      // submit color: blue
@@ -142,23 +114,22 @@ function gl_initBuffers () {
 //   ];
 var triangleVertices = [
     -1, -Math.sqrt(6)/3, -Math.sqrt(3)/3,//
-    0, 0, 0,
+    0, 1, 1,
     1, -Math.sqrt(6) / 3, -Math.sqrt(3)/3,//
-    1, 0, 0,
+    1, 0, 1,
     0, -Math.sqrt(6) / 3, 2*Math.sqrt(3)/3,//
-    1, 1, 0,
+    1, 0, 1,
     0, Math.sqrt(6)/3, 0,//
-    0, 1, 0,
+    1, 1, 0,
 ];
 
 
-   // Building Vertex Buffer Object - WebGL vertex array
-   _triangleVertexBuffer = gl_ctx.createBuffer();                // *******
+
+   _triangleVertexBuffer = gl_ctx.createBuffer();               
    gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _triangleVertexBuffer);
    gl_ctx.bufferData(gl_ctx.ARRAY_BUFFER, new Float32Array(triangleVertices), gl_ctx.STATIC_DRAW);
 
 
-   // Triangle faces array
    // var triangleFaces = [0, 1, 2];
    var triangleFaces = [
     0, 1, 2, 
@@ -167,12 +138,11 @@ var triangleVertices = [
     0, 1, 3
    ];
 
-   _triangleFacesBuffer = gl_ctx.createBuffer();                     // *******
+   _triangleFacesBuffer = gl_ctx.createBuffer();                     
    gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _triangleFacesBuffer);
    gl_ctx.bufferData(gl_ctx.ELEMENT_ARRAY_BUFFER, new Uint16Array(triangleFaces), gl_ctx.STATIC_DRAW);
 }
 
-// ==================================================================== //
 
 function gl_setMatrix () {
    _matrixProjection = MATRIX.getProjection(40, gl_canvas.width/gl_canvas.height, 1, 100);
@@ -182,16 +152,13 @@ function gl_setMatrix () {
    MATRIX.translateZ(_matrixView, zoomRatio);
 }
 
-// ==================================================================== //
+
 
 function gl_draw() {
-   // set the color to transparent
    gl_ctx.clearColor(0.0, 0.0, 0.0, 0.0);
-   // enable Depth buffer test and set depth buffer comparison function
    gl_ctx.enable(gl_ctx.DEPTH_TEST);
    gl_ctx.depthFunc(gl_ctx.LEQUAL);
 
-   // set the clear value for the depth buffer to 1
    gl_ctx.clearDepth(1.0);
 
    var timeOld = 0;
@@ -211,38 +178,25 @@ function gl_draw() {
 
       timeOld = time;
 
-      // set the drawing area on the canvas and clear it
       gl_ctx.viewport(0.0, 0.0, gl_canvas.width, gl_canvas.height);
       gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT | gl_ctx.DEPTH_BUFFER_BIT);
 
-      // set projection matrix. _matrixProjection is not set yet.
-      // It is a javascript array of 1 dimension with 16 floats
       gl_ctx.uniformMatrix4fv(_PosMatrix, false, _matrixProjection);
       gl_ctx.uniformMatrix4fv(_MovMatrix, false, _matrixMovement);
       gl_ctx.uniformMatrix4fv(_ViewMatrix, false, _matrixView);
-      // drawing is here - use these points for next drawing
-      // gl_ctx.vertexAttribPointer(variable, dimension, type, normalize, total vertex size in bytes, offset)
       gl_ctx.vertexAttribPointer(_position, 3, gl_ctx.FLOAT, false, 4*(3+3), 0);
       gl_ctx.vertexAttribPointer(_color, 3, gl_ctx.FLOAT, false, 4*(3+3), 3*4);
 
       gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _triangleVertexBuffer);
       gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _triangleFacesBuffer);
 
-      // draw the triangle
-      //gl_ctx.drawElements(gl_ctx.TRIANGLES, 3, gl_ctx.UNSIGNED_SHORT, 0);
-
-      // draw cube
       gl_ctx.drawElements(gl_ctx.TRIANGLES, 4*3, gl_ctx.UNSIGNED_SHORT, 0);
 
-      // drawing is finished - show the render
       gl_ctx.flush();
-      // redraws the scene as soon as ready
       window.requestAnimationFrame(animate);
    };
 
-   // launch animate for the first time
    if(!running){
-      // launch animate for the first time
       animate(0);
       running=true;
    }
--- WebGL/2/js/main.js
+++ WebGL/2/js/main.js
@@ -51,8 +51,6 @@ function getImage() {
    img3 = document.getElementById('img3').checked;
 }
 
-// ==================================================================== //
-
 function gl_getContext (canvas) {
    try {
       var ctx = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
@@ -66,15 +64,8 @@ function gl_getContext (canvas) {
    return ctx;
 }
 
-// ==================================================================== //
-
-// Declare the shaders. They are pieces of code compiled by WebGL and
-// executed on the graphics device. They are written in GLSL.
 function gl_initShaders () {
-   // position of the point - 0. is Z and 1. is W
-   // PosMatrix is uniform variable - its value is constant while rendering an object
-   // MovMatrix is the movement matrix of the triangle
-   // gl_position -> we move position with MovMatrix before projecting it
+
    var vertexShader = "\n\
       attribute vec3 position;\n\
       uniform mat4 PosMatrix;\n\
@@ -87,7 +78,6 @@ function gl_initShaders () {
          vUV = uv;\n\
       }";
 
-   // set black color
    var fragmentShader = "\n\
       precision mediump float;\n\
       uniform sampler2D sampler;\n\
@@ -96,7 +86,6 @@ function gl_initShaders () {
          gl_FragColor = texture2D(sampler, vUV);\n\
       }";
 
-   // this function is used to compile a shader
    var getShader = function(source, type, typeString) {
       var shader = gl_ctx.createShader(type);
       gl_ctx.shaderSource(shader, source);
@@ -109,47 +98,31 @@ function gl_initShaders () {
       return shader;
    };
 
-   // Compile the vertex and fragment shaders
    var shader_vertex = getShader(vertexShader, gl_ctx.VERTEX_SHADER, "VERTEX");
    var shader_fragment = getShader(fragmentShader, gl_ctx.FRAGMENT_SHADER, "FRAGMENT");
 
-
-   // Create the Shader program.
-   // Shader program is a combination of a vertex and fragment shaders.
    var SHADER_PROGRAM = gl_ctx.createProgram();
    gl_ctx.attachShader(SHADER_PROGRAM, shader_vertex);
    gl_ctx.attachShader(SHADER_PROGRAM, shader_fragment);
 
-
-   // Linking of the shader program to the WebGL context - gl_ctx,
-   // in order to match the shader variables to javascript variables
    gl_ctx.linkProgram(SHADER_PROGRAM);
 
-   // Link PosMatrix\MovMatrix\ViewMatrix GLSL variables to
-   // _PosMatrix\_MovMatrix\_ViewMatrix javascript variables
-   // Uniforms do not need to be enabled like attributes
    _PosMatrix = gl_ctx.getUniformLocation(SHADER_PROGRAM, "PosMatrix");
    _MovMatrix = gl_ctx.getUniformLocation(SHADER_PROGRAM, "MovMatrix");
    _ViewMatrix = gl_ctx.getUniformLocation(SHADER_PROGRAM, "ViewMatrix");
 
-   // position GLSL variable links to _position variable
-
    _sampler = gl_ctx.getUniformLocation(SHADER_PROGRAM, "sampler");
    _uv = gl_ctx.getAttribLocation(SHADER_PROGRAM, "uv");
    _position = gl_ctx.getAttribLocation(SHADER_PROGRAM, "position");    // *******
-   // enable GLSL attributes variables
+
    gl_ctx.enableVertexAttribArray(_uv);
    gl_ctx.enableVertexAttribArray(_position);
 
-   // linking is over - tells WebGL context to use SHADER_PROGRAM for rendering.
    gl_ctx.useProgram(SHADER_PROGRAM);
    gl_ctx.uniform1i(_sampler, 0);
 }
 
-// ==================================================================== //
-
 function gl_initBuffers () {
-   // Point coordinates array of the triangle
 //   var triangleVertices = [
 //      -1, -1, 0,    // bottom left
 //       0, 0, 1,      // submit color: blue
@@ -160,44 +133,42 @@ function gl_initBuffers () {
 //   ];
    var triangleVertices = [
       -1, -1, -1,    0, 0,
-      1, -1, -1,    1, 0,
-      1,  1, -1,    1, 1,
+       1, -1, -1,    1, 0,
+       1,  1, -1,    1, 1,
       -1,  1, -1,    0, 1,
 
-      -1, -1, 1,    0, 0,
-      1, -1, 1,    1, 0,
-      1,  1, 1,    1, 1,
-      -1,  1, 1,    0, 1,
+      -1, -1, 1,     0, 0,
+       1, -1, 1,     1, 0,
+       1,  1, 1,     1, 1,
+      -1,  1, 1,     0, 1,
 
       -1, -1, -1,    0, 0,
-      -1, 1, -1,    1, 0,
-      -1,  1, 1,    1, 1,
-      -1,  -1, 1,    0, 1,
+      -1,  1, -1,    1, 0,
+      -1,  1,  1,    1, 1,
+      -1, -1,  1,    0, 1,
 
-      1, -1, -1,    0, 0,
-      1, 1, -1,    1, 0,
-      1,  1, 1,    1, 1,
-      1,  -1, 1,    0, 1,
+      1, -1, -1,     0, 0,
+      1,  1, -1,     1, 0,
+      1,  1,  1,     1, 1,
+      1, -1,  1,     0, 1,
 
       -1, -1, -1,    0, 0,
-      -1, -1, 1,    1, 0,
-      1,  -1, 1,    1, 1,
-      1,  -1, -1,    0, 1,
+      -1, -1,  1,    1, 0,
+       1, -1,  1,    1, 1,
+       1, -1, -1,    0, 1,
 
-      -1, 1, -1,    0, 0
-      -1, 1, 1,    1, 0,
-      1,  1, 1,    1, 1,
-      1,  1, -1,    0, 1
+      -1, 1, -1,     0, 0
+      -1, 1,  1,     1, 0,
+       1, 1,  1,     1, 1,
+       1, 1, -1,     0, 1
    ];
 
 
-   // Building Vertex Buffer Object - WebGL vertex array
-   _triangleVertexBuffer = gl_ctx.createBuffer();                // *******
+   _triangleVertexBuffer = gl_ctx.createBuffer();              
    gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _triangleVertexBuffer);
    gl_ctx.bufferData(gl_ctx.ARRAY_BUFFER, new Float32Array(triangleVertices), gl_ctx.STATIC_DRAW);
 
 
-   // Triangle faces array
    // var triangleFaces = [0, 1, 2];
    var triangleFaces = [
       0,1,2,
@@ -219,12 +190,10 @@ function gl_initBuffers () {
       20,22,23
    ];
 
-   _triangleFacesBuffer = gl_ctx.createBuffer();                     // *******
+   _triangleFacesBuffer = gl_ctx.createBuffer();                   
    gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _triangleFacesBuffer);
    gl_ctx.bufferData(gl_ctx.ELEMENT_ARRAY_BUFFER, new Uint16Array(triangleFaces), gl_ctx.STATIC_DRAW);
 }
-
-// ==================================================================== //
 
 function gl_setMatrix () {
    _matrixProjection = MATRIX.getProjection(40, gl_canvas.width/gl_canvas.height, 1, 100);
@@ -233,8 +202,6 @@ function gl_setMatrix () {
 
    MATRIX.translateZ(_matrixView, zoomRatio);
 }
-
-// ==================================================================== //
 
 function gl_initTexture() {
    var img = new Image();
@@ -267,16 +234,10 @@ function gl_initTexture() {
 
 }
 
-
-
 function gl_draw() {
-   // set the color to transparent
    gl_ctx.clearColor(0.0, 0.0, 0.0, 0.0);
-   // enable Depth buffer test and set depth buffer comparison function
    gl_ctx.enable(gl_ctx.DEPTH_TEST);
    gl_ctx.depthFunc(gl_ctx.LEQUAL);
-
-   // set the clear value for the depth buffer to 1
    gl_ctx.clearDepth(1.0);
 
    var timeOld = 0;
@@ -298,12 +259,9 @@ function gl_draw() {
 
       timeOld = time;
 
-      // set the drawing area on the canvas and clear it
       gl_ctx.viewport(0.0, 0.0, gl_canvas.width, gl_canvas.height);
       gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT | gl_ctx.DEPTH_BUFFER_BIT);
 
-      // set projection matrix. _matrixProjection is not set yet.
-      // It is a javascript array of 1 dimension with 16 floats
       gl_ctx.uniformMatrix4fv(_PosMatrix, false, _matrixProjection);
       gl_ctx.uniformMatrix4fv(_MovMatrix, false, _matrixMovement);
       gl_ctx.uniformMatrix4fv(_ViewMatrix, false, _matrixView);
@@ -313,27 +271,20 @@ function gl_draw() {
          gl_ctx.bindTexture(gl_ctx.TEXTURE_2D, _cubeTexture.webglTexture);
       }
 
-      // drawing is here - use these points for next drawing
       gl_ctx.vertexAttribPointer(_position, 3, gl_ctx.FLOAT, false, 4*(3+2), 0);
       gl_ctx.vertexAttribPointer(_uv, 2, gl_ctx.FLOAT, false, 4*(3+2), 3*4);
 
       gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _triangleVertexBuffer);
       gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _triangleFacesBuffer);
 
-      // draw the triangle
-      //gl_ctx.drawElements(gl_ctx.TRIANGLES, 3, gl_ctx.UNSIGNED_SHORT, 0);
-
-      // draw cube
       gl_ctx.drawElements(gl_ctx.TRIANGLES, 5*2*3, gl_ctx.UNSIGNED_SHORT, 0);
 
-      // drawing is finished - show the render
       gl_ctx.flush();
-      // redraws the scene as soon as ready
+
       window.requestAnimationFrame(animate);
    };
 
    if(!running){
-      // launch animate for the first time
       animate(0);
       running=true;
    }
